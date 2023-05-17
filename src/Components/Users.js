cc7ec7224ae88dfe9b5582b27cd545c640c9896c
nGhart <107068059+nGhart@users.noreply.github.com>
2023-05-17 17:27:53 +0000

    Update Users.js

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -1,45 +1,24 @@
-import React, { Component } from 'react';
-import Card from 'react-bootstrap/Card';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
+import SingleUser from './SingleUser';
 
-class Users extends Component {
-    deleteUser = (e) => {
-        e.preventDefault();
-        this.props.deleteUser(this.props.items.id);
-    }
-    render() {
-        return (
-            <Container>
-                <Row>
-                    <h1 className='users'>Users</h1>
-                    {this.props.items.map((item) => (
-
-                        <Col xs={4} key={item.id} deleteUser={this.props.deleteUser} className='box'>
-
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Body>
-                                    <Card.Title>Name: {item.name}</Card.Title>
-                                    <Card.Text>
-                                        Email: {item.email}
-                                    </Card.Text>
-                                    <Card.Text>
-                                        Gen: {item.gen}
-                                    </Card.Text>
-                                    <div className='userModification'>
-                                        <Button variant="primary" className='userButton'>Edit</Button>
-                                        <Button variant="primary" className='userButton' onClick={() => this.props.deleteUser(item)}>Delete</Button>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    ))}
-                </Row>
-            </Container>
-        );
-    }
+const Users = (props) => {
+    return (
+        <Container>
+            <Row>
+                <h1 className='users'>Users</h1>
+                <Col className='userDisplay'>
+                    {props.usersData.map((user) => {
+                        return <SingleUser userInfo={user} key={user.id} deleteUser={props.deleteUser} editUser={props.editUser} />
+                    })
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
 }
+
 
 export default Users;
