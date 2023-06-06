8f86eff1d4a7347090092159a22a4ce2daf0c2b4
nGhart <107068059+nGhart@users.noreply.github.com>
2023-06-06 18:02:24 +0000

    Update UsersForm.js

--- src/Components/UsersForm.js
+++ src/Components/UsersForm.js
@@ -1,80 +1,62 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-export default class UserForm
-    extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            persons: [
-                {
-                    name: "",
-                    email: "",
-                    gen: ""
-                },
-            ]
-        }
-    }
+const UsersForm = (props) => {
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        gen: ""
+    });
 
-    handleChange = (e) => {
+    const handleChange = (e) => {
         e.preventDefault();
-        this.setState({ [e.target.name]: e.target.value });
+        setState({ ...state, [e.target.name]: e.target.value });
     };
-    handleSubmit = (e) => {
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        this.props.newUser(this.state);
-        this.setState({
+        props.addUser(state);
+        setState({
             name: "",
             email: "",
-            gen: "",
+            gen: ""
         });
-        console.log(this.state.persons);
     };
-    render() {
-        return (
-            <div
-                className='signupForm'>
-                <h1>Register</h1>
-                <form
-                    onSubmit={this.handleSubmit}>
-                    <div>
-                        <label>Name: </label>
-                        <input
-                            type="text"
-                            name="name"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label>Email: </label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <label>Gen: </label>
-                        <input
-                            type="text"
-                            name="gen"
-                            value={this.state.gen}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <button className='button' >Submit</button>
-                </form>
-                {this.state.persons.map((item, index) => {
-                    return (
-                        <div
-                            key={index}>
-                        </div>
-                    )
-                })}
-            </div>
-        )
-    }
-}
 
+    return (
+        <div className='signupForm'>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Name: </label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={state.name}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input
+                        type="email"
+                        name="email"
+                        value={state.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label>Gen: </label>
+                    <input
+                        type="text"
+                        name="gen"
+                        value={state.gen}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button className='button'>Submit</button>
+            </form>
+        </div>
+    );
+};
 
+export default UsersForm
